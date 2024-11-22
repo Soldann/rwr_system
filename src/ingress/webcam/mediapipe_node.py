135423a11d794da26c9cd91d8c181af9c47501d6
James-Earl-Jone <cedric.bugnon@bluewin.ch>
2024-11-22 18:31:18 +0100

    fixed mediapipe bugs

--- src/ingress/webcam/mediapipe_node.py
+++ src/ingress/webcam/mediapipe_node.py
@@ -6,13 +6,14 @@ from std_msgs.msg import Float32MultiArray, MultiArrayDimension, MultiArrayLayou
 from geometry_msgs.msg import PoseStamped, Point, Quaternion
 from webcam_mediapipe_ingress import MediaPipeTracker
 from faive_system.src.common.utils import numpy_to_float32_multiarray
+import cv2
 
 
 class MediapipeNode(Node):
     def __init__(self):
         super().__init__("rokoko_node")
 
-        self.tracker = MediaPipeTracker(show=False)
+        self.tracker = MediaPipeTracker(show=True)
         self.tracker.start()
 
         ingress_period = 0.005  # Timer period in seconds
@@ -23,17 +24,23 @@ class MediapipeNode(Node):
         )
 
     def timer_publish_cb(self):
-
+        if self.tracker.frame is not None:
+            cv2.imshow('MediaPipe Hands', self.tracker.frame)
+            if cv2.waitKey(5) & 0xFF == 27:
+                self.tracker.stop()
         keypoint_positions = self.tracker.get_keypoint_positions()
-        wait_cnt = 1
-        while (keypoint_positions is None):
-            if (wait_cnt % 10):
-                print("waiting for hand tracker")
-            wait_cnt+=1
-            keypoint_positions = self.tracker.get_keypoint_positions()
+        if keypoint_positions is not None:
+            print(keypoint_positions)
+        # wait_cnt = 1
+        # while (keypoint_positions is None):
+        #     if (wait_cnt % 10):
+        #         print("waiting for hand tracker")
+        #     wait_cnt+=1
+        #     keypoint_positions = self.tracker.get_keypoint_positions()
 
-        keypoint_positions_msg = numpy_to_float32_multiarray(keypoint_positions)
-        self.ingress_mano_pub.publish(keypoint_positions_msg)
+
+            keypoint_positions_msg = numpy_to_float32_multiarray(keypoint_positions)
+            self.ingress_mano_pub.publish(keypoint_positions_msg)
 
 
 def main(args=None):
